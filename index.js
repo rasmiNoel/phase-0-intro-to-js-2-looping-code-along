6b16783545ceb762b4bd3f01064707622101a9c4
rasmi <rasminoel@gmail.com>
2023-09-23 21:48:25 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const name = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(name) {
+    for (let i = 0; i < name.length; i++) {
+        console.log(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return writeCards(name.length);
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
